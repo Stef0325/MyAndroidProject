b003bca4c0a41a860fa6834ab35f277c7c069624
stef <zhangcheng0325@outlook.com>
2023-02-11 14:08:33 +0800

    modify the clock application

--- BroadcastTest/app/src/main/java/cn/edu/snnu/zc/broadcasttest/MainActivity.java
+++ BroadcastTest/app/src/main/java/cn/edu/snnu/zc/broadcasttest/MainActivity.java
@@ -2,8 +2,11 @@ package cn.edu.snnu.zc.broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         networkChangeReceiver=new NetworkChangeReceiver();
 //        调用registerReceiver方法动态注册广播接收者
         registerReceiver(networkChangeReceiver,intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
